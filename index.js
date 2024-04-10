19eb59f0232b69c3169143092825b76047cf0975
rabehrb203 <165825164+rabehrb203@users.noreply.github.com>
2024-04-10 03:30:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
 const puppeteer = require("puppeteer-core");
 
 const app = express();
